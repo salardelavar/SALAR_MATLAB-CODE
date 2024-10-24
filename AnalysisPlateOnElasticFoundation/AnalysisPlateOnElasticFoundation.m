b4ab2be41e402efd10adabfe6d1729105091454b
Salar Delavar Qashqai <44362930+salardelavar@users.noreply.github.com>
2024-10-24 10:35:03 +0330

    Update AnalysisPlateOnElasticFoundation.m

--- AnalysisPlateOnElasticFoundation/AnalysisPlateOnElasticFoundation.m
+++ AnalysisPlateOnElasticFoundation/AnalysisPlateOnElasticFoundation.m
@@ -1,6 +1,6 @@
 %**************************************************************%
 %                  >> IN THE NAME OF ALLAH <<                  %
-% This program analysis Plate Element on elastic foundation    %           
+% This program analysis Soil and elastic foundation Interaction%           
 % by finite difference Method                                  %
 %--------------------------------------------------------------%
 %      This program is written by salar delavar ghashghaei     %
